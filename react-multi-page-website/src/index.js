dd46b8cabb65f23006907f682d7c710a2f059338
marthamaybennett <marthamaybennett123@gmail.com>
2023-04-20 14:56:18 +0100

    setting up pages and bug-blasting

--- react-multi-page-website/src/index.js
+++ react-multi-page-website/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -14,7 +14,9 @@ import {
   Post,
 } from "./components";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+
+ root.render (
   <Router>
     <Navigation />
     <Routes>
